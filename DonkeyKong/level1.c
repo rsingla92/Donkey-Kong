2a193e9a7a987b4519dd09c8be986c2d404fe293
laurenf720 <laurenf720@gmail.com>
2013-09-26 19:10:08 -0700

    mario moving along first plane
    gonna map out more planes in a bit :)

--- DonkeyKong/level1.c
+++ DonkeyKong/level1.c
@@ -17,17 +17,25 @@ typedef struct {
 typedef struct {
 	Point start;
 	Point end;
-	int slope;
+	float slope;
 } Plane;
 
 static const Plane floors[] =
 {
 	{{0, 91}, {205, 91}, 0},
-	{{137,62}, {205, 62},0 }
+	{{137,62}, {205, 62},0 },
+	{{206,92},{296,95}, -3/90 },
 };
 
-int find_floor(int x){
+#define NUM_FLOORS (sizeof(floors)/sizeof(floors[0]))
 
+int find_floor(int x){
+	int i;
+	for (i = 0; i < NUM_FLOORS; i++){
+		if(x >= floors[i].start.x && x <= floors[i].end.x)
+			return (floors[i].start.y + floors[i].slope*x);
+	}
+	return -1;
 }
 
 void draw_level1() {
@@ -41,5 +49,6 @@ void draw_level1() {
 	drawBackground();
 	swap_buffers();
 	drawBackground();
-
 }
+
+
--- DonkeyKong/main.c
+++ DonkeyKong/main.c
@@ -4,27 +4,23 @@
 #include "display.h"
 #include "level1.h"
 #include "background.h"
-#include "level1.h"
 #include "sys/alt_alarm.h"
 #include "sys/alt_timestamp.h"
 
 #define NUM_FILES 44
 
-static char* file_list[NUM_FILES] = {
-		"4B.BMP", "B1.BMP", "B2.BMP", "B3.BMP", "B4.BMP", "B5.BMP", "DK1.BMP",
-		"DK2.BMP", "DK3.BMP", "DK4.BMP", "DK5.BMP", "DK6.BMP", "DK7.BMP", "DK8.BMP",
-		"DK9.BMP", "DK10.BMP", "DK11.BMP", "FIRE.BMP", "FIRE1.BMP", "FIRE2.BMP",
-		"FIRE3.BMP", "HMR.BMP", "M1.BMP", "M2.BMP", "M3.BMP", "M4.BMP", "M5.BMP",
-		"M6.BMP", "M7.BMP", "M8.BMP", "M9.BMP", "M10.BMP", "M11.BMP", "M12.BMP",
-		"M13.BMP", "M14.BMP", "M15.BMP", "P1.BMP", "P2.BMP", "PP1.BMP", "PP2.BMP",
-		"PP3.BMP", "PURSE.BMP", "UMBRLA.BMP"
-};
-
-static 	BitmapHandle* bmp;
+static char* file_list[NUM_FILES] = { "4B.BMP", "B1.BMP", "B2.BMP", "B3.BMP",
+		"B4.BMP", "B5.BMP", "DK1.BMP", "DK2.BMP", "DK3.BMP", "DK4.BMP",
+		"DK5.BMP", "DK6.BMP", "DK7.BMP", "DK8.BMP", "DK9.BMP", "DK10.BMP",
+		"DK11.BMP", "FIRE.BMP", "FIRE1.BMP", "FIRE2.BMP", "FIRE3.BMP",
+		"HMR.BMP", "M1.BMP", "M2.BMP", "M3.BMP", "M4.BMP", "M5.BMP", "M6.BMP",
+		"M7.BMP", "M8.BMP", "M9.BMP", "M10.BMP", "M11.BMP", "M12.BMP",
+		"M13.BMP", "M14.BMP", "M15.BMP", "P1.BMP", "P2.BMP", "PP1.BMP",
+		"PP2.BMP", "PP3.BMP", "PURSE.BMP", "UMBRLA.BMP" };
+static BitmapHandle* bmp;
 static alt_u32 ticks_per_sec;
 static alt_u32 num_ticks;
-
-static alt_32 update(void *context);
+alt_32 update(void *context);
 
 int main(void) {
 	init_display();
@@ -33,7 +29,8 @@ int main(void) {
 
 	clear_display();
 
-	if (sd_dev == NULL) return 1;
+	if (sd_dev == NULL)
+		return 1;
 
 	printf("Card connected.\n");
 
@@ -45,50 +42,47 @@ int main(void) {
 	int x = 0;
 	int y = 72;
 
-	draw_level1();
+	// Draw the background to both buffers.
+	drawBackground();
+	swap_buffers();
+	drawBackground();
 
-	int count = 0;
-	int x = 0;
-	int y = 72;
+	printf ("floor is at: %d\n",find_floor(1));
+	printf ("floor is at: %d\n",find_floor(0));
+	printf ("floor is at: %d\n",find_floor(280));
 
 	ticks_per_sec = alt_ticks_per_second();
-	num_ticks = ticks_per_sec/30;
-	printf("num: %d\n\n ticks per sec: %d\n\n", num_ticks, ticks_per_sec);
-	alt_alarm *update_alarm;
-	alt_alarm_start(update_alarm, num_ticks, update, NULL);
-
+	num_ticks = ticks_per_sec / 30;
+	alt_alarm *update_alarm = malloc(sizeof(alt_alarm));
+	alt_alarm_start(update_alarm, num_ticks, update, (void*)0);
+	// function calls here are tricky to predict
+	// printf only worked if only one printf statement was put
+	while (true) {
+		// Check events here.
+	}
 
 	return 0;
 }
 
-static alt_32 update(void *context)
-{
-	//alt_timestamp_start();
-	//int end_time;
-	//int start_time = (int) alt_timestamp();
+alt_32 update(void *context) {
 
 	static int x = 0;
-	int y = 72;
+	static int y = 91;
 	colour col = { 0x1F, 0x00, 0x1F };
 
-	if (x > 320)
-	{
+	if (x > 320) {
 		x = 0;
-	}
-	else
-	{
-		drawBackgroundSection(x-1, y, x, y + bmp->bmp_info_header->height);
+	} else {
+		drawBackgroundSection(x - 1, y, x, y + bmp->bmp_info_header->height);
 	}
 
 	draw_bmp(bmp, x, y, true, col);
 	x++;
+	y = find_floor(x);
+	if (y == -1)
+		y = 91;
 	swap_buffers();
 
-	printf("hello2\n");
-
-	//end_time = (int)alt_timestamp();
-		//printf("start %d\n end %d\n", start_time, end_time);
-		//printf("time taken: %d clock ticks\n", end_time-start_time);
-		//printf("            %f seconds \n", (float)(end_time-start_time)/(float)alt_timestamp_freq())
+	return 1;
 
 }
