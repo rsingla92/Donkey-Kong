dee07399ad53c7e7dddd7fede4cf1958e3ab7438
lordy1992 <jlord_2006@hotmail.com>
2013-10-06 17:30:07 -0700

    Adding load screen module

---
+++ DonkeyKong/load_screen.c
@@ -0,0 +1,7 @@
+/*
+ * load_screen.c
+ *
+ *  Created on: 2013-10-06
+ *      Author: Jeremy
+ */
+
---
+++ DonkeyKong/load_screen.h
@@ -0,0 +1,12 @@
+/*
+ * load_screen.h
+ *
+ *  Created on: 2013-10-06
+ *      Author: Jeremy
+ */
+
+#ifndef LOAD_SCREEN_H_
+#define LOAD_SCREEN_H_
+
+
+#endif /* LOAD_SCREEN_H_ */
