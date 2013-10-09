ee9500f17a79d4450590429c4fd876e2cf060b2f
rsingla92 <rsingla92@gmail.com>
2013-10-08 18:43:44 -0700

    main menu fo real

---
+++ DonkeyKong/mainMenu.h
@@ -0,0 +1,13 @@
+/*
+ * mainMenu.h
+ *
+ *  Created on: Oct 8, 2013
+ *      Author: singla
+ */
+
+#ifndef MAINMENU_H_
+#define MAINMENU_H_
+
+void updateMainMenu(void);
+
+#endif /* MAINMENU_H_ */
