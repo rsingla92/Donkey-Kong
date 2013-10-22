9bbb79e0dc1b5c97e0f835ef7fcba587834794f4
Ricky Singla <rsingla92@gmail.com>
2013-10-22 16:28:45 -0700

    Update input.h

--- DonkeyKong/input.h
+++ DonkeyKong/input.h
@@ -10,6 +10,7 @@
 
 #define buttons (volatile char *) BUTTONS_BASE
 
+// Flags for the buttons
 typedef struct s_controller_buttons {
 	unsigned char UP_ARROW;
 	unsigned char RIGHT_ARROW;
@@ -25,9 +26,16 @@ typedef struct s_controller_buttons {
 	unsigned char R_BUTTON;
 } controller_buttons;
 
+// For previous and next controller
 void copyController(controller_buttons* copy_to, controller_buttons copy_from);
+
+// Determine if an individual buttonNum
 unsigned char getButton(int buttonNum);
+
+// Get all the controller buttons 
 controller_buttons getControllerButtons(unsigned short dat);
+
+// Set up the needed variables for the controller
 void initController(controller_buttons* controller);
 
 #endif /* INPUT_H_ */
